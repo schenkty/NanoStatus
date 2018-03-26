f9a12ebed199bfb42d61a67b1b41f7db0734ed7a
Ty Schenk <7807169+schenkty@users.noreply.github.com>
2018-03-26 12:08:30 -0700

    fixed issue with price being stuck and cleaned up format

--- RaiMoon/AppDelegate.swift
+++ RaiMoon/AppDelegate.swift
@@ -13,14 +13,16 @@ import Alamofire
 class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
     
-    var item : NSStatusItem? = nil
+    var item: NSStatusItem? = nil
+    var btc: NSMenuItem? = nil
+    var percentChange1h: NSMenuItem? = nil
+    var percentChange24h: NSMenuItem? = nil
+    var percentChange7d: NSMenuItem? = nil
+    var marketCap: NSMenuItem? = nil
+    var rank: NSMenuItem? = nil
+    var updated: NSMenuItem? = nil
     
-    var btc : NSMenuItem? = nil
-    var percentChange1h : NSMenuItem? = nil
-    var percentChange24h : NSMenuItem? = nil
-    var percentChange7d : NSMenuItem? = nil
-    var marketCap : NSMenuItem? = nil
-    var rank : NSMenuItem? = nil
+    var localTimeZoneAbbreviation: String { return TimeZone.current.abbreviation() ?? "" }
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         item = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
@@ -33,49 +35,88 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         percentChange7d = NSMenuItem(title: "", action: nil, keyEquivalent: "")
         marketCap = NSMenuItem(title: "", action: nil, keyEquivalent: "")
         rank = NSMenuItem(title: "", action: nil, keyEquivalent: "")
+        updated = NSMenuItem(title: "", action: nil, keyEquivalent: "")
         
         let menu = NSMenu()
-        menu.addItem(btc!)
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(percentChange1h!)
-        menu.addItem(percentChange24h!)
-        menu.addItem(percentChange7d!)
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(marketCap!)
-        menu.addItem(rank!)
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(title: "Refresh Data", action: #selector(fetchPrice), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Developer: Ty Schenk", action: nil, keyEquivalent: ""))
         menu.addItem(NSMenuItem.separator())
         menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quit), keyEquivalent: ""))
         item?.menu = menu
         
-        fetchPrice()
+        refreshPrice()
         
         // Pull every 5 mins. Data Source only updates every 5 mins
         Timer.scheduledTimer(timeInterval: 300, target: self, selector: #selector(fetchPrice), userInfo: nil, repeats: true)
+        Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(refreshPrice), userInfo: nil, repeats: true)
+    }
+    
+    @objc func refreshPrice() {
+        if Connectivity.isConnectedToInternet {
+            if item?.title == "Fetching price..." {
+                fetchPrice()
+            }
+        } else {
+            return
+        }
     }
 
     @objc func fetchPrice() {
-        print("Fetching price...")
+        if !Connectivity.isConnectedToInternet {
+            return
+        }
         
         Alamofire.request("https://api.coinmarketcap.com/v1/ticker/nano/").responseJSON { response in
             if let data = response.result.value {
                 if  (data as? [[String : AnyObject]]) != nil {
                     if let dictionaryArray = data as? Array<Dictionary<String, AnyObject?>> {
-                        let usd = dictionaryArray[0]["price_usd"] as? String
+                        self.item?.menu = NSMenu()
                         
-                        self.item?.title = "NANO: $\(usd ?? "???")"
+                        let usd = dictionaryArray[0]["price_usd"] as? String ?? "0.00"
+                        let symbol = dictionaryArray[0]["name"] as? String
+                        
+                        self.item?.title = "\(symbol ?? "-"): \(self.formatCurrency(value: Double(usd)!))"
                         
                         self.btc?.title = "\(dictionaryArray[0]["price_btc"] as? String ?? "???") BTC"
                         self.percentChange1h?.title = "1h: \(dictionaryArray[0]["percent_change_1h"] as? String ?? "???")%"
                         self.percentChange24h?.title = "24h: \(dictionaryArray[0]["percent_change_24h"] as? String ?? "???")%"
                         self.percentChange7d?.title = "7d: \(dictionaryArray[0]["percent_change_7d"] as? String ?? "???")%"
-                        self.marketCap?.title = "Market Cap: $\(dictionaryArray[0]["market_cap_usd"] as? String ?? "???")"
                         self.rank?.title = "Rank: \(dictionaryArray[0]["rank"] as? String ?? "???")"
+                        
+                        let mcString = dictionaryArray[0]["market_cap_usd"] as? String ?? "0.0"
+                        let mc = Double(mcString)!
+                        self.marketCap?.title = "Market Cap: \(self.formatCurrency(value: mc))"
+                        
+                        let updated = dictionaryArray[0]["last_updated"] as? String ?? "1514764800"
+                        let stamp = Double(updated)!
+                        let date = Date(timeIntervalSince1970: stamp)
+                        let dateFormatter = DateFormatter()
+                        dateFormatter.timeZone = TimeZone(abbreviation: self.localTimeZoneAbbreviation)
+                        dateFormatter.locale = NSLocale.current
+                        dateFormatter.dateFormat = "M/dd/yyyy HH:mm a"
+                        let strDate = dateFormatter.string(from: date)
+                        self.updated?.title = "Updated: \(strDate)"
+                        
+                        let menu = NSMenu()
+                        menu.addItem(self.btc!)
+                        menu.addItem(NSMenuItem.separator())
+                        menu.addItem(self.percentChange1h!)
+                        menu.addItem(self.percentChange24h!)
+                        menu.addItem(self.percentChange7d!)
+                        menu.addItem(NSMenuItem.separator())
+                        menu.addItem(self.marketCap!)
+                        menu.addItem(self.rank!)
+                        menu.addItem(NSMenuItem.separator())
+                        menu.addItem(self.updated!)
+                        menu.addItem(NSMenuItem.separator())
+                        menu.addItem(NSMenuItem(title: "Developer: Ty Schenk", action: nil, keyEquivalent: ""))
+                        menu.addItem(NSMenuItem.separator())
+                        menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quit), keyEquivalent: ""))
+                        self.item?.menu = menu
                     }
                 }
             } else {
                 let error = (response.result.value  as? [[String : AnyObject]])
+                print("data pull failed")
                 print(error as Any)
             }
         }
@@ -83,6 +124,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     @objc func quit() {
         NSApplication.shared.terminate(self)
+    }
+    
+    // MARK: Check Network
+    class Connectivity {
+        class var isConnectedToInternet:Bool {
+            return NetworkReachabilityManager()!.isReachable
+        }
+    }
+    
+    func formatCurrency(value: Double) -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.maximumFractionDigits = 2
+        formatter.locale = Locale(identifier: Locale.current.identifier)
+        let result = formatter.string(from: value as NSNumber)
+        return result!
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
